87ac00007c4b38b90e25b29594b0873bfff8ae8a
Manuel-Iriarte <iriarte.manuel@yahoo.es>
2016-12-07 18:15:52 -0300

    Add project files.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Call()
+        {
+            return PartialView("_PartialFooter");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Call(CallMe callMe)
+        {
+            if (ModelState.IsValid)
+            {
+                ModelState.Clear();
+                callMe.CallMeMessage = callMe.CallMeMessage + " i was on the server";
+            }
+            return PartialView("_PartialFooter", callMe);
+        }
+    }
+}
